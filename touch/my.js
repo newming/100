4e0b4e5b99012aac8dd1cec01159305dba5e5e3b
newming <a977527479@sina.com>
2016-05-31 23:25:55 +0800

    eeee

--- touch/my.js
+++ touch/my.js
@@ -5,8 +5,10 @@ $('#btn').click(function() {
 $('.img-contain img').click(function() {
 	// var zMax = 0;
 	// console.log($('.panzoom').has('img').length);
+	var num = $('#paly').children('img').length;
+	console.log(num);
 	var newUrl = $(this).attr('src');
-	var newImg = "<img src='" + newUrl +"' class='panzoom'>";
+	var newImg = "<img src='" + newUrl +"' class='panzoom' z-index= '" + num + "'>";
 
 	// if ($('section').has('img').length == 0) {
 		$('section').append(newImg);
@@ -23,4 +25,28 @@ $('.img-contain img').click(function() {
 		$('.panzoom').panzoom({
 		});
   })();
+});
+
+$('#paly').click(function(event,zIndex) {
+	var x = event.target;
+	if ($(x).hasClass('panzoom')) {
+		var oldIndex = $(x).css('z-index');
+		// console.log('tupian');
+		var num = $('#paly').children('img').length - 1;
+		// console.log(num);
+		$(x).css('z-index', num);
+		var otherx = $(x).siblings();
+		console.log(otherx)
+		var other = $(x).siblings().length;
+		for (var i = 0; i < other ; i++) {
+			var otherIndex = $(x).siblings()[i];
+			var y = $(otherIndex).css('z-index');
+			// console.log(otherIndex);
+			// console.log(y);
+			if (y >= oldIndex ) {
+				y--;
+				$(otherIndex).css('z-index', y);
+			}
+		}
+	}
 });
